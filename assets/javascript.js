7c064121b256f32410c2ea2503cc6531446a5008
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-06-28 16:14:47 -0500

    tasks able to store and load

--- assets/javascript.js
+++ assets/javascript.js
@@ -11,15 +11,28 @@ function displayDateTime() {
 
 $(".saveBtn").on("click", function() {
     var saveTaskId =$(this).attr("data-hour");
-    var taskValue = $("#"+saveTaskId).val();
+    var taskValue = $("#"+saveTaskId).val().trim();
     var taskObject = {
         key: saveTaskId,
         value: taskValue
     };
-    tasks.push(taskObject);
+
+    var keyExists = false;
+
+    // check if current tasks array contains the key. if found, overwrite it
+   tasks.forEach((task) => {
+    if(task.key === saveTaskId ) {
+        task.value= taskValue;
+        keyExists = true;
+    }
+   })
+
+   if(!keyExists) {
+       tasks.push(taskObject);
+   }
+
     storeTasks();
 });
-
 
 
 
@@ -31,20 +44,33 @@ var loadTasks = function () {
   
     // if nothing in localStorage, create a new object to track all task status arrays
     if (!tasks) {
-      tasks = {
-        key: saveTaskId,
-        taskValue
-      };
+      tasks = [];
     }
+
+    console.log(tasks);
+
+    // for (let index = 0; index < tasks.length; index++) {
+       
+    //     var keyId = tasks[index].key;
+    //     var valueText = tasks[index]
+        
+    // }
+
+    tasks.forEach((task) => {
+        var keyId = task.key;
+        var valueText = task.value;
+
+        $("#"+keyId).val(valueText);
+    })
   
     // loop over object properties
-    $.each(tasks, function (list, arr) {
-      // then loop over sub-array
-      arr.forEach(function (task) {
-        createTask(task.text, task.date, list);
-      });
-    });
-  };
+    // $.each(tasks, function (key, value) {
+    //   // then loop over sub-array
+    //   arr.forEach(function (task) {
+    //     createTask(task.text, task.date, list);
+    //   });
+    // });
+};
   
 
 var storeTasks = function () {
